67c16c7bee037db6a7738c36a5ca9ad3c9a3b1da
Jesse Tsang <Jessetsang@outlook.com>
2017-10-04 15:51:11 -0400

    Create RemoteInterface

---
+++ src/common/BankServerInterface.java
@@ -0,0 +1,17 @@
+package common;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.HashMap;
+
+public interface BankServerInterface extends Remote
+{
+	public Boolean createAccount(String firstName, String lastName, String address, int phone, int branch) throws RemoteException;
+	public Boolean editRecord(String customerID, String fieldName, String newValue) throws RemoteException;
+	public HashMap<String, Integer> getAccountCount() throws RemoteException;
+	public void getUDPIPData(int port) throws RemoteException;
+	
+	public void deposit(String customerID, int amt) throws RemoteException;
+	public void withdraw(String customerID, int amt) throws RemoteException;
+	public void getBalance(String customerID) throws RemoteException;
+}
