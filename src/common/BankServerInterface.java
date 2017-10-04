b1353dc91e20241cc1c62b1a1698b1f370109907
Jesse Tsang <Jessetsang@outlook.com>
2017-10-04 15:59:19 -0400

    Added extra functions for getAccountCount() and login()

--- src/common/BankServerInterface.java
+++ src/common/BankServerInterface.java
@@ -9,9 +9,11 @@ public interface BankServerInterface extends Remote
 	public Boolean createAccount(String firstName, String lastName, String address, int phone, int branch) throws RemoteException;
 	public Boolean editRecord(String customerID, String fieldName, String newValue) throws RemoteException;
 	public HashMap<String, Integer> getAccountCount() throws RemoteException;
-	public void getUDPIPData(int port) throws RemoteException;
 	
 	public void deposit(String customerID, int amt) throws RemoteException;
 	public void withdraw(String customerID, int amt) throws RemoteException;
 	public void getBalance(String customerID) throws RemoteException;
+	
+	public void login(String customerID, String password) throws RemoteException;
+	public void getUDPIPData(int port) throws RemoteException;
 }
