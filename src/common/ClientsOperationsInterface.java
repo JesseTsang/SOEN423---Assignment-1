fba7b40086a1958184f957234699695669f923d1
Jesse Tsang <Jessetsang@outlook.com>
2017-10-04 16:16:53 -0400

    Seperate BankServerInterface into to interface with meaningful name.
    BankServerInterface splited into two function-oriented interfaces: one
    for manager operations, one for client operations.

---
+++ src/common/ClientsOperationsInterface.java
@@ -0,0 +1,12 @@
+package common;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ClientsOperationsInterface extends Remote
+{
+	//ClientOperationsInterface
+	public void deposit(String customerID, int amt) throws RemoteException;
+	public void withdraw(String customerID, int amt) throws RemoteException;
+	public void getBalance(String customerID) throws RemoteException;
+}
--- src/common/ManagerOperationInterface.java
+++ src/common/ManagerOperationInterface.java
@@ -4,16 +4,10 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.HashMap;
 
-public interface BankServerInterface extends Remote
+public interface ManagerOperationInterface extends Remote
 {
+	//ManagerOperationsInterface
 	public Boolean createAccount(String firstName, String lastName, String address, int phone, int branch) throws RemoteException;
 	public Boolean editRecord(String customerID, String fieldName, String newValue) throws RemoteException;
 	public HashMap<String, Integer> getAccountCount() throws RemoteException;
-	
-	public void deposit(String customerID, int amt) throws RemoteException;
-	public void withdraw(String customerID, int amt) throws RemoteException;
-	public void getBalance(String customerID) throws RemoteException;
-	
-	public void login(String customerID, String password) throws RemoteException;
-	public void getUDPIPData(int port) throws RemoteException;
 }
