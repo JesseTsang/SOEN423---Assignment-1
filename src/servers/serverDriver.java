2df16e386ed0d3cde6269f2b8367cdfb408685f9
Jesse Tsang <Jessetsang@outlook.com>
2017-10-09 01:18:36 -0400

    Create Test Case for BankServerImpl Class

--- src/servers/serverDriver.java
+++ src/servers/serverDriver.java
@@ -2,14 +2,32 @@ package servers;
 
 import common.BankServerImpl;
 import domain.BranchID;
+import domain.EditRecordFields;
 
-public class serverDriver {
-
+public class serverDriver 
+{
 	public static void main(String[] args) 
-	{
+	{		
 		try
 		{
-			new BankServerImpl(BranchID.QC, 2964);
+			String firstname1 = "Paul";
+			String lastName1 = "Netto";
+			String address1 = "4965, Heritage Drive";
+			String phone1 = "450-123-4567";
+			String customerID1 = "BCCN4567";
+			BranchID branch1 = BranchID.QC;
+			
+			EditRecordFields editAddress1 = EditRecordFields.address;
+			String newAddressValue1 = "3921 rue Ellice";
+			EditRecordFields editPhone1 = EditRecordFields.phone;
+			String newPhoneValue1 = "438-123-123"; //Should produce error
+			
+			BankServerImpl serverTest1 = new BankServerImpl(BranchID.QC, 2964);
+			
+			serverTest1.createAccount(firstname1, lastName1, address1, phone1, customerID1, branch1);
+			//serverTest1.deposit(customerID1, 1000);
+			serverTest1.editRecord(customerID1, editAddress1, newAddressValue1);
+			serverTest1.editRecord(customerID1, editPhone1, newPhoneValue1);			
 		}
 		catch (Exception e)
 		{
