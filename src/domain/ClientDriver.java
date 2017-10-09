a0157728c22ab56989f76050e5e5800edf26a6cb
Jesse Tsang <Jessetsang@outlook.com>
2017-10-09 01:19:42 -0400

    Test Case for Client class

--- src/domain/ClientDriver.java
+++ src/domain/ClientDriver.java
@@ -5,11 +5,11 @@ public class ClientDriver
 
 	public static void main(String[] args) throws Exception
 	{
-		String firstName1 = "Ka Man";
-		String lastName1 = "Tsang";		
+		String firstName1 = "James";
+		String lastName1 = "Castillo";		
 		String address1 = "Rue HelloWorld";
 		String phoneNum1 = "514-123-1234";
-		String clientNumber1 = "BCC1234";
+		String clientNumber1 = "BCCC1234";
 		BranchID branch1 = BranchID.BC;
 		
 		Client client1 = new Client(firstName1, lastName1, address1, phoneNum1, clientNumber1, branch1);
