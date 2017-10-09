439e55a032fc17aff4be01a0297726a74b7dfb2d
Jesse Tsang <Jessetsang@outlook.com>
2017-10-09 01:21:49 -0400

    Change editRecord argument field
    from String to EditRecordField enum to lower error chance.

--- src/common/BankServerInterface.java
+++ src/common/BankServerInterface.java
@@ -5,16 +5,23 @@ import java.rmi.RemoteException;
 import java.util.HashMap;
 
 import domain.BranchID;
+import domain.EditRecordFields;
 
 public interface BankServerInterface extends Remote
 {
 	//ManagerOperationsInterface
 	public Boolean createAccount(String firstName, String lastName, String address, String phone, String customerID,BranchID branch) throws RemoteException;
-	public Boolean editRecord(String customerID, String fieldName, String newValue) throws RemoteException;
+	public Boolean editRecord(String customerID, EditRecordFields fieldName, String newValue) throws RemoteException;
 	public HashMap<String, Integer> getAccountCount() throws RemoteException;
 	
 	//ClientOperationsInterface
 	public void deposit(String customerID, int amt) throws RemoteException;
 	public void withdraw(String customerID, int amt) throws RemoteException;
 	public void getBalance(String customerID) throws RemoteException;
+	
+	//MiscOperations
+	public int getLocalAccountCount();
+	
+	//CoreOperations
+	public void getUDPData(int port) throws RemoteException;
 }
