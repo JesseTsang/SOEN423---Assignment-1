b024f411f7631bf8b400dff4c9b0a0776ac84fef
Jesse Tsang <Jessetsang@outlook.com>
2017-10-10 00:10:12 -0400

    Finished Admin and Customer clients.

--- src/client/AdminClient.java
+++ src/client/AdminClient.java
@@ -21,13 +21,23 @@ public class AdminClient
 	private String customerID;
 	private BranchID branchID;
 	
-	public AdminClient(BranchID branchID) throws RemoteException, NotBoundException
+	private static final int ACCOUNT_TYPE_POS = 2;
+	
+	public AdminClient(String customerID, BranchID branchID) throws RemoteException, NotBoundException
 	{
+		char accountType = (char)customerID.charAt(ACCOUNT_TYPE_POS);
+		if(accountType != 'M')
+		{
+			throw new RemoteException ("Login Error: This client is for managers only.");
+		}
+		
 		this.branchID = branchID;
 		registry = LocateRegistry.getRegistry(BANK_HOST, BANK_PORT);
+		
+		System.out.println("Login Sucessed. | Customer ID: " + this.customerID + " | Branch ID: " + this.branchID.toString());
 	}
 	
-	public void createAccountRecord(String firstName, String lastName, String address, String phone, String customerID, BranchID branch) 
+	public synchronized void createAccountRecord(String firstName, String lastName, String address, String phone, String customerID, BranchID branch) 
 			throws AccessException, RemoteException, NotBoundException
 	{	
 		BankServerInterface bankServer = (BankServerInterface) registry.lookup(this.branchID.toString());
@@ -39,11 +49,11 @@ public class AdminClient
 		}
 		else
 		{
-			System.out.println("Error: Account Unable to Create. Please consult server log.");
+			System.out.println("Account Creation Error: Account Unable to Create. Please consult server log.");
 		}
 	}
 	
-	public void editRecord(String customerID, EditRecordFields fieldName, String newValue) throws AccessException, RemoteException, NotBoundException
+	public synchronized void editRecord(String customerID, EditRecordFields fieldName, String newValue) throws AccessException, RemoteException, NotBoundException
 	{
 		BankServerInterface bankServer = (BankServerInterface) registry.lookup(this.branchID.toString());
 		boolean result = bankServer.editRecord(customerID, fieldName, newValue);
@@ -54,11 +64,11 @@ public class AdminClient
 		}
 		else
 		{
-			System.out.println("Error: Account Unable to Modify. Please consult server log.");
+			System.out.println("Account Edit Error: Account Unable to Modify. Please consult server log.");
 		}
 	}
 	
-	public HashMap<String, String> getAccountCount() throws AccessException, RemoteException, NotBoundException
+	public synchronized HashMap<String, String> getAccountCount() throws AccessException, RemoteException, NotBoundException
 	{
 		BankServerInterface bankServer = (BankServerInterface) registry.lookup(this.branchID.toString());
 		HashMap<String, String> result = bankServer.getAccountCount();
@@ -68,8 +78,9 @@ public class AdminClient
 	public static void main(String args[])
 	{
 		try
-		{
-			AdminClient testClient1 = new AdminClient(BranchID.BC);
+		{	
+			String testAdminID = "BCMJ1234";
+			AdminClient testClient1 = new AdminClient(testAdminID, BranchID.BC);
 		}
 		catch (RemoteException | NotBoundException e)
 		{
